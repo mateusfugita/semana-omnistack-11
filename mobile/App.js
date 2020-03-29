a6d27164e1d25f2c64576003dfdbf4c9bb23b27e
Mateus Fugita <mateushiwao@gmail.com>
2020-03-29 13:09:28 -0300

    Mobile

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+//expo init [nome do app]
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes></Routes>
+  );
+}
