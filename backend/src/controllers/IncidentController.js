a0291a9c1b89f85669efd4e056907d04d7168b31
Mateus Fugita <mateushiwao@gmail.com>
2020-03-29 13:07:24 -0300

    fix error

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -12,7 +12,7 @@ module.exports = {
             .offset((page - 1) * 5)
             .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
         
-        response.headers('X-Total-Count', count['count(*)']);
+        response.header('X-Total-Count', count['count(*)']);
         
         return response.json(incidents);
     },
