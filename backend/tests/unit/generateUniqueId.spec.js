37836e184825e3c230337c127ba7793793d61a23
Mateus Fugita <mateushiwao@gmail.com>
2020-04-12 12:35:55 -0300

    advanced configurations

---
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,15 @@
+/**
+ * npm install jest
+ * npx jest --init
+ * npm test
+ */
+
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
+describe('Generate Unique ID', () => {
+    it('should generate an unique ID', () => {
+        const id = generateUniqueId();
+
+        expect(id).toHaveLength(8);
+    })
+})
